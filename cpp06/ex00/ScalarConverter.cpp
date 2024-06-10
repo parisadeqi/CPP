d8fffa093d3c9c986cf4409a68c5aa4a5ab1b189
Parisa Sadeghi <pariisadeqii@gmail.com>
2024-06-10 18:17:53 +0200

    adding ex01

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: psadeghi <psadeghi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/06 17:13:30 by psadeghi          #+#    #+#             */
-/*   Updated: 2024/06/06 18:38:04 by psadeghi         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ScalarConverter.cpp                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: psadeghi <psadeghi@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/06 17:13:30 by psadeghi      #+#    #+#                 */
+/*   Updated: 2024/06/10 14:13:10 by parisasadeq   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter(const std::string& argv1){
-	std::cout << "Original constructor of ScalarConverter got called." << std::endl;
+ScalarConverter::ScalarConverter(const std::string& literal){
+	std::cout << "Original constructor of ScalarConverter" << literal <<  " got called." << std::endl;
 }
 
 ScalarConverter::ScalarConverter (const ScalarConverter& copy) {
@@ -27,19 +27,80 @@ ScalarConverter::~ScalarConverter () {
 
 ScalarConverter&	ScalarConverter::operator= (const ScalarConverter& var)
 {
+	(void)var;
 	return (*this);
 }
 
-e_type ScalarConverter::detectType(std::string argv1) {
+void ScalarConverter::convert(std::string literal) 
+{
+	std::string specialTypes[6] = 
+	{
+		"-inff", "+inff", "nanf",
+		"-inf", "+inf", "nan"
+	};
+	std::string toChar = "";
+	int toInt = 0;
+	float toFloat = 0;
+	double toDouble = 0;
 
-	// char
-	if (argv1.size() == 1 && std::isprint(argv1[0]) && !std::isdigit(argv1[0])) {
-		type = CHAR;
-		// char c = argv1[0];
-		// std::cout << "char: '" << c << "'" << std::endl;
-		// std::cout << "int: " << static_cast<int>(c) << std::endl;
-		// std::cout << "float: " << static_cast<float>(c) << "f" << std::endl;
-		// std::cout << "double: " << static_cast<double>(c) << std::endl;
-		// return;
+	if (literal.size() == 1 && std::isprint(literal[0]) && !std::isdigit(literal[0])) {
+		char c = literal[0];
+		std::cout << "char: '" << c << "'" << std::endl;
+		std::cout << "int: " << static_cast<int>(c) << std::endl;
+		std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+		std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
+		return;
+	}
+	toInt = std::atoi(literal.c_str());
+	if (literal[literal.length() - 1] == 'f') 
+	{
+		toFloat = std::atof(literal.c_str());
+		toDouble = static_cast<double>(toFloat);
+	} else 
+	{
+		toDouble = std::atof(literal.c_str());
+		toFloat = static_cast<float>(toDouble);
+	}
+	for (int i = 0; i < 6; ++i) 
+	{
+		if (literal == specialTypes[i]) 
+		{
+			toChar = "imposible";
+			break;
+		}
+	}
+	if (toChar == "" && std::isprint(toInt)) 
+	{
+		toChar = "'";
+		toChar += static_cast<char>(toInt);
+		toChar += "'";
+	} 
+	else if (toChar == "") 
+	{
+		toChar = "Non displayable";
+	}
+	std::cout << "char: " << toChar << std::endl;
+	if (toChar == "imposible") 
+		std::cout << "int: imposible" << std::endl;
+	else 
+		std::cout << "int: " << toInt << std::endl;
+
+	if (toChar == "imposible" && toFloat == 0) 
+	{
+		std::cout << "float: imposible" << std::endl;
+		std::cout << "double: imposible" << std::endl;
+	} 
+	else 
+	{
+		if (toChar != "imposible" && toFloat - static_cast<int>(toFloat) == 0) 
+		{
+			std::cout << "float: " << toFloat << ".0f" << std::endl;
+			std::cout << "double: " << toDouble << ".0" << std::endl;
+		} 
+		else 
+		{
+			std::cout << "float: " << toFloat << "f" << std::endl;
+			std::cout << "double: " << toDouble << std::endl;
+		}
 	}
 }
