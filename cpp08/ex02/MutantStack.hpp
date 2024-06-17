29cca76140c325d00dba2bef97223c20c866c4f7
Parisa Sadeghi <pariisadeqii@gmail.com>
2024-06-17 21:35:01 +0200

    adding basics of ex02  cpp08

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   MutantStack.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parisasadeqi <parisasadeqi@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/17 20:43:36 by parisasadeq   #+#    #+#                 */
+/*   Updated: 2024/06/17 20:44:21 by parisasadeq   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+
+
+#endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parisasadeqi <parisasadeqi@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/17 20:39:20 by parisasadeq   #+#    #+#                 */
+/*   Updated: 2024/06/17 20:39:40 by parisasadeq   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
